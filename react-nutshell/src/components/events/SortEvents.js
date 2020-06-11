62d433b244379ec9ef8761ba6414810eaa7b4b29
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-06-11 11:35:42 -0400

    stretch

--- react-nutshell/src/components/events/SortEvents.js
+++ react-nutshell/src/components/events/SortEvents.js
@@ -1,40 +1,51 @@
-// filterEventsByMonth(){
-//     return fetch(`http://localhost:8088/events/`)
-//     .then(r => r.json())
-//     .then(parsedEvents => {
-//         const monthNames = {
-//             1:"January", 
-//             2:"February", 
-//             3:"March", 
-//             4:"April", 
-//             5:"May", 
-//             6:"June", 
-//             7:"July", 
-//             8:"August", 
-//             9:"September", 
-//             10:"October", 
-//             11:"November", 
-//             12:"December"
-//         }
-//         const monthNamesArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-//         const dateArray = parsedEvents.map(event => event.date)
-//         const monthArray = dateArray.sort((a,b) => a.split("-")[1] - b.split("-")[1]).map(date => parseInt(date.split("-")[1]))
-//         console.log(monthArray)
-//         console.log(monthNames["January"])
-//         console.log(monthNamesArray[1])
-//         console.log(monthArray[0])
-//         for (let i = 0; i < parsedEvents.length; i++){
-//             for (let n = 0; i < monthArray.length; n++){
-//                 console.log("Inside second for loop")
-//                 // if(monthNames[n + 1] === monthArray[i]){
-//                 //     console.log(`${monthNames[n + 1]}`)
-//                 // }
-//             // if(monthArray[i] === monthNames[i]){
-//             //     console.log(`${monthArray[i]} equals ${monthNames[n]}`)
-//             //     document.querySelector("#events-container").innerHTML += `${monthNames[n]}`
-//             // }
-//             }
-//         }
-//     })
-// }
+    // object key is 1 value is Jan
+    // getEventsByMonth(){
+    //     return fetch(`http://localhost:8088/events/?userId=${sessionStorage.getItem("userId")}`)
+    //     .then(r => r.json())
+    //     .then(parsedEvents => {
+    //         const monthNames = {
+    //             1:"January", 
+    //             2:"February", 
+    //             3:"March", 
+    //             4:"April", 
+    //             5:"May", 
+    //             6:"June", 
+    //             7:"July", 
+    //             8:"August", 
+    //             9:"September", 
+    //             10:"October", 
+    //             11:"November", 
+    //             12:"December"
+    //         }
+    //         //Sort date and time in ascending order
+    //         parsedEvents.sort((a, b) => {          
+    //             if (a.date === b.date) {
+    //                return parseInt(a.time) - parseInt(b.time);
+    //             }
+    //             return a.date > b.date ? 1 : -1;
+    //          });
+
+    //         //Create today's date
+    //         var today = new Date()                    
+    //         today = `${today.getFullYear()}-${(today.getMonth()+ 1) > 9 ? "" + (today.getMonth()+ 1): "0" + (today.getMonth()+ 1)}-${today.getDate() > 9 ? "" + today.getDate(): "0" + today.getDate()}`
+
+    //         for (const monthNum in monthNames){
+    //             const eventsPerMonth = parsedEvents.filter(event => {
+    //                 let perMonth = false
+
+    //                 if(event.date >= today && parseInt(event.date.split("-")[1]) === parseInt(monthNum) ){
+    //                     perMonth = true
+    //                 }
+    //                 return perMonth
+    //             })
+    //             console.log(eventsPerMonth)
+    //             console.log(eventsPerMonth.length)
+    //             console.log(monthNames[monthNum])
+    //             if(eventsPerMonth.length !== 0){
+    //                 event_domPrinter.printAccordion(monthNames[monthNum], eventsPerMonth, eventsPerMonth.length)
+    //             }
+    //         }
+    //     })
+                
+    // }
 
